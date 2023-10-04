1736830f27208c837aaec9f42a7ace84f5498d39
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-10-04 16:36:39 +0300

    rightbar

--- src/component/rightbar/RightBar.js
+++ src/component/rightbar/RightBar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Search from './search/Search';
 import Topics from './topics/Topics';
+import WhoFollow from './who-to-follow/WhoFollow';
+import Footer from './footer/Footer';
 
 const RightBar = () => {
     return (
         <div className='w-[350px] mr-2.5 '>
             <Search/>
             <Topics/>
+            <WhoFollow/>
+            <Footer/>
         </div>
     );
 }
