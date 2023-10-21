3903312527941f21dfce6c1ce55382f04fb48ce7
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-10-22 00:20:11 +0300

    Register-Page

--- src/component/register/Footer.js
+++ src/component/register/Footer.js
@@ -1,11 +1,74 @@
 
 
 import React from 'react';
+import './LeftPart.css';
+
 
 const Footer = () => {
     return (
-        <div>
-            FOOTER
+        <div className='footer'>
+           <nav>
+            <a className="w-[56px] h-[16px] " href="https://about.twitter.com/">
+                <span> Hakkında</span>
+            </a>
+            <a className="w-[119px] h-[16px] " href="https://help.twitter.com/using-x/download-the-x-app">
+                <span> X uygulamasını indir</span>
+            </a>
+            <a className="w-[90px] h-[16px]" href="https://help.twitter.com/">
+                <span> Yardım Merkezi</span>
+            </a>
+            <a className="w-[88px] h-[16px]" href="https://twitter.com/tos">
+                <span> Hizmet Şartları</span>
+            </a>
+            <a className="w-[96px] h-[16px]" href="https://twitter.com/privacy">
+                <span> Gizlilik Politikası</span>
+            </a>
+            <a className="w-[91px] h-[16px]"  href="https://support.twitter.com/articles/20170514">
+                <span> Çerez Politikası</span>
+            </a>
+            <a className="w-[43px] h-[16px]"  href="https://legal.twitter.com/imprint.html">
+                <span> Imprint</span>
+            </a>
+            <a className="w-[71px] h-[16px]"  href="https://help.twitter.com/resources/accessibility">
+                <span> Erişilebilirlik</span>
+            </a>
+            <a className="w-[81px] h-[16px]"  href="https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html?ref=web-twc-ao-gbl-adsinfo&utm_source=twc&utm_medium=web&utm_campaign=ao&utm_content=adsinfo">
+                <span> Reklam Bilgisi</span>
+            </a>
+            <a className="w-[26px] h-[16px]"  href="https://blog.twitter.com/">
+                <span> Block</span>
+            </a>
+            <a className="w-[40px] h-[16px]"  href="https://status.twitterstat.us/">
+                <span> Durum</span>
+            </a>
+            <a className="w-[41px] h-[16px]"  href="https://careers.twitter.com/">
+                <span> Kariyer</span>
+            </a>
+            <a className="w-[102px] h-[16px]"  href="https://about.twitter.com/press/brand-assets">
+                <span> Marka Kaynakları</span>
+            </a>
+            <a className="w-[44px] h-[16px]"  href="https://ads.twitter.com/?ref=gl-tw-tw-twitter-advertise">
+                <span> Reklam</span>
+            </a>
+            <a className="w-[62px] h-[16px]"  href="https://marketing.twitter.com/">
+                <span> Pazarlama</span>
+            </a>
+            <a className="w-[94px] h-[16px]"  href="https://business.twitter.com/?ref=web-twc-ao-gbl-twitterforbusiness&utm_source=twc&utm_medium=web&utm_campaign=ao&utm_content=twitterforbusiness">
+                <span> İşletmeler İçin X</span>
+            </a>
+            <a className="w-[71px] h-[16px]"  href="https://developer.twitter.com/">
+                <span> Geliştiriciler</span>
+            </a>
+            <a className="w-[29px] h-[16px]"  href="https://twitter.com/i/directory/profiles">
+                <span> Dizin</span>
+            </a>
+            <a className="w-[41px] h-[16px]"  href="https://twitter.com/settings">
+                <span> Ayarlar</span>
+            </a>
+            <div className="w-[92px] h-[16px] ">
+            <span >© 2023 X Corp.</span>
+            </div>
+           </nav>
         </div>
     );
 }
--- src/component/register/LeftPart.js
+++ src/component/register/LeftPart.js
@@ -4,11 +4,14 @@
 import React from 'react';
 
 import './RightPart.css';
+import './LeftPart.css';
 
 const LeftPart = () => {
     return (
         <div className='content-left'>
-            LEFT
+           <div className='context-left'>
+           <svg viewBox="0 0 24 24" aria-hidden="true" className='left-svg'><g><path fill="currentcolor" d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path></g></svg>
+           </div>
         </div>
     );
 }
